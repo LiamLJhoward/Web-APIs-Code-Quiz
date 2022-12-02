31ee36d04d834e67e84b28e17e2d99a3d3f41a18
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2022-12-02 16:11:56 +0000

    timer complete

---
+++ assets/js/logic.js
@@ -0,0 +1,39 @@
+let timerElement = document.querySelector("#time");
+let startButton = document.querySelector("#start"); 
+
+let timer;
+let timerCount;
+
+
+// Start Game
+function startGame() {
+    timerCount = 60;
+    // Stops start button from being pressed again
+    startTimer()
+    console.log("Start button pressed");
+}
+
+// Start timer
+function startTimer() {
+    timer = setInterval(function() {
+        timerCount--;
+        timerElement.textContent = timerCount;
+            if (timerCount >= 0) {
+                // tests win condition 
+
+                // Clears interval and stops timer
+                clearInterval(timer);
+            
+    }
+    
+    // If time has ran out
+    if (timerCount === 0) {
+        // Clears interval and sets lose function
+        clearInterval(timer);
+        //lose
+    } 
+}, 1000);
+}
+
+// Attach event listener to start button
+startButton.addEventListener("click", startGame);
