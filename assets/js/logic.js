6248a01f1ecedebdb5c35b93cbc93c88925c7a72
LiamLJhoward <115423162+LiamLJhoward@users.noreply.github.com>
2022-12-04 12:28:45 +0000

    changed the question layout

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,14 +2,10 @@ let timerElement = document.querySelector("#time");
 let startButton = document.querySelector("#start"); 
 let startScreen = document.querySelector("#start-screen");
 let questionTitle = document.querySelector("#question-title");
-
+let questions = document.querySelector("#questions");
 
 let timer;
 let timerCount;
-
-
-
-
 
 
 
@@ -17,12 +13,11 @@ let timerCount;
 function startGame() {
     //Hides the start screen 
     startScreen.style.visibility = 'hidden';
-    // Questions Pop Up
-    questionTitle.style.visibility = 'show';
     timerCount = 60;
-    // Stops start button from being pressed again
+    // Start timer
     startTimer()
-    console.log("Start button pressed");
+    // Start questions
+    startQuestions()
 }
 
 // Start timer
@@ -56,62 +51,51 @@ function startTimer() {
 startButton.addEventListener("click", startGame);
 
 
+// Start questions
+function startQuestions() {
+    
+
+}
 
 
 
+// Questions & answers
+let question = {
+        questionOne: "All right Jim, your quarterlies look very good. How are things going at the library?",
+        questionTwo: "Shall I play for you Pa Rum Pum Pum Pum, I have no gifts for you Pa Rum Pum Pum Pum.",
+        questionThree: "You guys gotta update your resumes, just like I'm doing.",
+        questionFour: "So what does downsizing actually mean?",
+        questionFive: "I bet it's gonna be me. It's probably gonna be me.",
+        questionSix: "I'll drink to that. I'm starting at a company this week.",
+        questionSeven: "I have a customer meeting.",
+        questionEight: "Michael, please. Can we talk about this memo?",  
+        questionNine: "Oh. Okay. And to you (starts clapping).",
+        questionTen: "You don't work in sales, do you?"
+}
 
-// Questions
-let questions = [
-    {
-        title: "All right Jim, your quarterlies look very good. How are things going at the library?",
-        choices:["David", "Michael", "Jan", "Dwight"],
-        answer: "Michael"
-    },
-    {
-        title: "Shall I play for you Pa Rum Pum Pum Pum, I have no gifts for you Pa Rum Pum Pum Pum.",
-        choices: ["Angela", "Kelly", "Dwight", "Creed"],
-        answer: "Dwight"
-    },
-    {
-        title: "You guys gotta update your resumes, just like I'm doing.",
-        choices: ["Ryan", "Angela", "Darrel", "Oscar"],
-        answer: "Oscar"
-    },
-    {
-        title: "So what does downsizing actually mean?",
-        choices: ["Kevin", "Phyllis", "Jan", "Meredith"] ,
-        answer: "Phyllis"
-    },
-    {
-        title: "I bet it's gonna be me. It's probably gonna be me.",
-        choices: ["Jim", "Pam", "Stanley", "Angela"],
-        answer: "Angela"
-    },
-    {
-        title: "I'll drink to that. I'm starting at a company this week.",
-        choices: ["Deangelo Vickers", "Nellie Bertram", "Robert California", "Charles Miner"],
-        answer: "Deangelo Vickers"
-    },
-    {
-        title: "I have a customer meeting.",
-        choices: ["David", "Kelly", "Jan", "Karen"],
-        answer: "Kelly"
-    },
-    {
-        title: "Michael, please. Can we talk about this memo?",
-        choices:["Meredith", "Kevin", "Stanley", "Creed"] ,
-        answer: "Meredith"
-    },
-    {
-        title: "Oh. Okay. And to you (starts clapping).",
-        choices:["Packer", "Gabe", "Erin", "Helene"] ,
-        answer: "Gabe"
-    },
-    {
-        title: "You don't work in sales, do you?",
-        choices: ["Robert California", "Jan", "Jo Bennet", "Danny Cordray"],
-        answer: "Robert California"
-    }
+let answerChoices = {
+    choiceOne:["David", "Michael", "Jan", "Dwight"],
+    choiceTwo: ["Angela", "Kelly", "Dwight", "Creed"],
+    choiceThree: ["Ryan", "Angela", "Darrel", "Oscar"],
+    choiceFour: ["Kevin", "Phyllis", "Jan", "Meredith"],
+    choiceFive: ["Jim", "Pam", "Stanley", "Angela"],
+    choiceSix: ["Deangelo Vickers", "Nellie Bertram", "Robert California", "Charles Miner"],
+    choiceSeven: ["David", "Kelly", "Jan", "Karen"],
+    choiceEight:["Meredith", "Kevin", "Stanley", "Creed"] ,
+    choiceNine:["Packer", "Gabe", "Erin", "Helene"] ,
+    choiceTen: ["Robert California", "Jan", "Jo Bennet", "Danny Cordray"],
+}
 
-]
+let answers = {
+    answerOne: "Michael",
+    answerTwo: "Dwight",
+    answerThree: "Oscar",
+    answerFour: "Phyllis",
+    answerFive: "Angela",
+    answerSix: "Deangelo Vickers",
+    answerSeven: "Kelly",
+    answerEight: "Meredith",
+    answerNine: "Gabe",
+    answerTen: "Robert California"
 
+}
